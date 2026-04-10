5111ed5e28f162274958555eb94be191df4f6224
xvzf_opt <nsandro77@proton.me>
2026-04-10 23:24:55 +0100

    Added util functions for converting strings and unicode

--- recon/utils/utils.py
+++ recon/utils/utils.py
@@ -16,3 +16,31 @@ def get_random_str(length):
     Generates a random string of the specified length, containing ASCII characters
     '''
     return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+
+def to_unicode_str(obj, encoding='utf-8'):
+    '''
+    Converts non-stringish types to unicode
+
+    :param obj: The object to convert
+    :type obj: object
+    :return: The converted string
+    :rtype: str
+    '''
+    if type(obj) not in (str, bytes):
+        obj = str(obj)
+    obj = to_unicode(obj, encoding)
+    return obj
+
+def to_unicode(obj, encoding='utf-8'):
+    '''
+    Converts bytes to unicode
+
+    :param obj: The bytes object to convert
+    :type obj: bytes
+    :return: The converted bytes object
+    :rtype: str
+    '''
+    if isinstance(obj, bytes):
+        obj = obj.decode(encoding)
+    return obj
+
