df072256af0b180fcfcfdd3adf969e00dab1a773
xvzf_opt <nsandro77@proton.me>
2026-04-18 00:40:18 +0100

    Interpreter updates
    * Added Interpreter Status enum property as a means to determine if an interpreter is running, or has been exited
    * Got back functionality working so that it's now possible to leave a module and return to the framework
    * Got options commands working, along with help and auto-completion
    * Got modules command auto-completion working
    * Ported across _list_options function to print available options, depending on context
    * Fixed prompt missing sign
    * Got module info command working
    * Got module goptions command working, with auto-complete and help
    * Got module options command working, with auto-complete and help

--- recon/core/interpreter/base.py
+++ recon/core/interpreter/base.py
@@ -8,7 +8,8 @@ from cmd import Cmd
 # =====================================================================================
 # Imports: Internal
 # =====================================================================================
-# from recon.core.recon_ngx import ReconNGXApp
+from recon.utils import utils
+
 
 # =====================================================================================
 # Base Command Interpreter Class
@@ -23,6 +24,10 @@ class BaseInterpreter(Cmd):
     # =====================================================================================
     RULER = '-'
     SPACER = '  '
+
+    STATUS_EXITED   = 0
+    STATUS_RUNNING  = 1
+    STATUS_RELOADED = 2
 
     # =====================================================================================
     # Functions
@@ -39,6 +44,7 @@ class BaseInterpreter(Cmd):
         super(BaseInterpreter, self).__init__()
         self._recon = recon
         self._console = console
+        self._status = None
 
         self._base_prompt = "[%s]" % self._recon.get_app_name()
 
@@ -48,6 +54,7 @@ class BaseInterpreter(Cmd):
         Starts the interpreter session
         '''
         self.print_banner()
+        self._status = self.STATUS_RUNNING
         self.cmdloop()
 
     def print_banner(self):
@@ -113,14 +120,81 @@ class BaseInterpreter(Cmd):
             self._console.print_exception()
 
     # =====================================================================================
-    # Command Functions
+    # Command Functions: Back/Exit
     # =====================================================================================
     def do_exit(self, params):
         '''
         Action Handler: exit
         '''
-        self._exit = 1
+        self._status = self.STATUS_EXITED
         return True
+
+    def do_back(self, params):
+        '''
+        Action Handler: Back
+        '''
+        self._status = self.STATUS_EXITED
+        return True
+
+    # =====================================================================================
+    # Command Do Functions: "options"
+    # =====================================================================================
+    def do_options(self, params):
+        '''Manages the current context options'''
+
+        # Check options subcommand was provided
+        if not params:
+            self.help_options()
+            return
+        arg, params = self._parse_params(params)
+
+        # Check subcommand is valid
+        if arg in self._get_subcommands('options'):
+            return getattr(self, '_do_options_'+arg)(params)
+        else:
+            self.help_options()
+
+    def _do_options_list(self, params):
+        '''Shows the current context options'''
+        self._list_options()
+
+    def _do_options_set(self, params):
+        '''Sets a current context option'''
+
+        # Parse option key and value
+        option, value = self._parse_params(params)
+        if not (option and value):
+            self._help_options_set()
+            return
+
+        # Get Workspace
+        workspace = self._recon.get_current_workspace()
+
+        # Check option is a valid, known Global Option
+        options = self._recon.get_options()
+        option_name = option.upper()
+        if option_name in options:
+            options[option_name] = value
+            print(f"{option_name} => {value}")
+            workspace.set_config_property(option_name, options=options)
+        else:
+            self._console.error('Invalid option name.')
+
+    def _do_options_unset(self, params):
+        '''Unsets a current context option'''
+
+        # Parse option key and value
+        option, value = self._parse_params(params)
+        if not option:
+            self._help_options_unset()
+            return
+
+        options = self._recon.get_options()
+        option_name = option.upper()
+        if option_name in options:
+            self._do_options_set(' '.join([option_name, 'None']))
+        else:
+            self._console.error('Invalid option name.')
 
     # =====================================================================================
     # Command Do Functions: "modules"
@@ -198,6 +272,109 @@ class BaseInterpreter(Cmd):
         mm.load_modules()
 
     # =====================================================================================
+    # Auto-completion Functions: modules
+    # =====================================================================================
+    def complete_modules(self, text, line, *ignored):
+        '''
+        Auto-completion for modules commands
+
+        :param text: The subcommand text to auto-complete, which has been typed so far
+        :type text: str
+        :param line: The entire line that has been typed so far
+        :type line: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+
+        arg, params = self._parse_params(line.split(' ', 1)[1])
+        subs = self._get_subcommands('modules')
+
+        # If directly matching sub-command found, auto-complete that
+        if arg in subs:
+            return getattr(self, '_complete_modules_'+arg)(text, params)
+
+        # Else return all available matching commands
+        return [sub for sub in subs if sub.startswith(text)]
+
+    def _complete_modules_search(self, text, *ignored):
+        '''
+        Auto-completion for modules command: search
+        Placeholder: currently we have nothing more to provide for this command
+
+        :param text: The subcommand text to auto-complete, which has been typed so far
+        :type text: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+        return []
+    # Auto-complete modules "reload" command in same way as search
+    _complete_modules_reload = _complete_modules_search
+
+    def _complete_modules_load(self, text, *ignored):
+        '''
+        Auto-completion for modules command: load
+        Searches all installed modules for those that match
+
+        :param text: The subcommand text to auto-complete, which has been typed so far
+        :type text: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+        mm = self._recon.get_module_manager()
+        return [x for x in mm.get_loaded_modules() if x.startswith(text)]
+
+    # =====================================================================================
+    # Auto-completion Functions: options
+    # =====================================================================================
+    def complete_options(self, text, line, *ignored):
+        '''
+        Auto-completion for options commands
+
+        :param text: The subcommand text to auto-complete, which has been typed so far
+        :type text: str
+        :param line: The entire line that has been typed so far
+        :type line: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+        arg, params = self._parse_params(line.split(' ', 1)[1])
+        subs = self._get_subcommands('options')
+
+        # If directly matching sub-command found, auto-complete that
+        if arg in subs:
+            return getattr(self, '_complete_options_'+arg)(text, params)
+
+        # Else return all available matching commands
+        return [sub for sub in subs if sub.startswith(text)]
+
+    def _complete_options_list(self, text, *ignored):
+        '''
+        Auto-completion for options command: list
+        Placeholder: currently we have nothing more to provide for this command
+
+        :param text: The subcommand text to auto-complete, which has been typed so far
+        :type text: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+        return []
+
+    def _complete_options_set(self, text, *ignored):
+        '''
+        Auto-completion for options command: set
+        Searches all global options for an option that matches
+
+        :param text: The option name to auto-complete, which has been typed so far
+        :type text: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+        return [x for x in self._recon.get_options() if x.startswith(text.upper())]
+    # Auto-complete options "unset" in same way as set
+    _complete_options_unset = _complete_options_set
+
+
+    # =====================================================================================
     # Command Help Functions
     # =====================================================================================
     def help_modules(self):
@@ -212,6 +389,31 @@ class BaseInterpreter(Cmd):
         print(getattr(self, '_do_modules_load').__doc__)
         print(f"{os.linesep}Usage: modules load <path>{os.linesep}")
 
+    def help_options(self):
+        print(getattr(self, 'do_options').__doc__)
+        print(f"{os.linesep}Usage: options <{'|'.join(self._get_subcommands('options'))}> [...]{os.linesep}")
+
+    def _help_options_set(self):
+        print(getattr(self, '_do_options_set').__doc__)
+        print(f"{os.linesep}Usage: options set <option> <value>{os.linesep}")
+
+    def _help_options_unset(self):
+        print(getattr(self, '_do_options_unset').__doc__)
+        print(f"{os.linesep}Usage: options unset <option>{os.linesep}")
+
+
+    # =====================================================================================
+    # Getters
+    # =====================================================================================
+    def get_status(self):
+        '''
+        Gets the interpreter's status
+
+        :returns: The interpreter's status enum int
+        :rtype: int
+        '''
+        return self._status
+
     # =====================================================================================
     # Setters
     # =====================================================================================
@@ -219,7 +421,7 @@ class BaseInterpreter(Cmd):
         '''
         Sets the current workspace name
         '''
-        self.prompt = "%s[%s] " % (self._base_prompt, name)
+        self.prompt = "%s[%s] > " % (self._base_prompt, name)
 
     # =====================================================================================
     # Helpers
@@ -246,6 +448,43 @@ class BaseInterpreter(Cmd):
             print('')
             self._console.alert('No modules enabled/installed.')
         print('')
+
+
+    def _list_options(self, options=None):
+        '''
+        Display the options that are available.
+        If a dictionary is passed in then this will be used, otherwise the interpreter will fall back to the global
+        options
+
+        :param options: The options to display
+        :type options: dict
+        '''
+
+        # Use run-time options if provided, otherwise use Global Options
+        if options is None:
+            options = self._recon.get_options()
+
+        # Copied across form recon-ng
+        if options:
+            pattern = f"{self.SPACER}%s  %s  %s  %s"
+            key_len = len(max(options, key=len))
+            if key_len < 4: key_len = 4
+            val_len = len(max([utils.to_unicode_str(options[x]) for x in options], key=len))
+            if val_len < 13: val_len = 13
+            print('')
+            print(pattern % ('Name'.ljust(key_len), 'Current Value'.ljust(val_len), 'Required', 'Description'))
+            print(pattern % (self.ruler*key_len, (self.ruler*13).ljust(val_len), self.ruler*8, self.ruler*11))
+            for key in sorted(options):
+                value = options[key] if options[key] != None else ''
+                reqd = 'no' if options.required[key] is False else 'yes'
+                desc = options.description[key]
+                print(pattern % (key.ljust(key_len), utils.to_unicode_str(value).ljust(val_len), utils.to_unicode_str(reqd).ljust(8), desc))
+            print('')
+        else:
+            print('')
+            print(f"{self.SPACER}No options available for this module.")
+            print('')
+
 
     def _parse_params(self, params):
         '''
--- recon/core/interpreter/module.py
+++ recon/core/interpreter/module.py
@@ -1,11 +1,14 @@
 # =====================================================================================
 # Imports: External
 # =====================================================================================
+import os
+import textwrap
 
 # =====================================================================================
 # Imports: Internal
 # =====================================================================================
 from .base import BaseInterpreter
+from recon.utils import utils
 
 # =====================================================================================
 # Module Interpreter Class
@@ -39,8 +42,7 @@ class ModuleInterpreter(BaseInterpreter):
         super(ModuleInterpreter, self).__init__(recon, console)
         self._module = module
         self._workspace = self._recon.get_current_workspace()
-        self.prompt = "%s[%s][%s] " % (self._base_prompt, self._workspace.get_name(), module.get_name())
-
+        self.prompt = "%s[%s][%s] > " % (self._base_prompt, self._workspace.get_name(), module.get_name())
 
     def start(self):
         '''
@@ -49,3 +51,243 @@ class ModuleInterpreter(BaseInterpreter):
         :note: Overrides base start to avoid banner print
         '''
         self.cmdloop()
+
+    # =====================================================================================
+    # Command Do Functions: "info"
+    # =====================================================================================
+    def do_info(self, params):
+        '''Shows details about the loaded module'''
+        print('')
+
+        # Print Basic Module information
+        for item in ['name', 'author', 'version']:
+            print(f"{item.title().rjust(10)}: {self._module.meta[item]}")
+
+        # Print any required Keys
+        if self._module.meta.get('required_keys'):
+            print(f"{'keys'.title().rjust(10)}: {', '.join(self._module.meta.get('required_keys'))}")
+        print('')
+
+        # Print Module Description
+        print('Description:')
+        print(f"{self.SPACER}{textwrap.fill(self._module.meta['description'], 100, subsequent_indent=self.SPACER)}")
+        print('')
+
+        # Print Module Option information
+        print('Options:', end='')
+        self._list_options(self._module._options)
+
+        # Print Module Source information (TODO TBC?)
+        if hasattr(self, '_default_source'):
+            print('Source Options:')
+            print(f"{self.SPACER}{'default'.ljust(15)}{self._default_source}")
+            print(f"{self.SPACER}{'<string>'.ljust(15)}string representing a single input")
+            print(f"{self.SPACER}{'<path>'.ljust(15)}path to a file containing a list of inputs")
+            print(f"{self.SPACER}{'query <sql>'.ljust(15)}database query returning one column of inputs")
+            print('')
+
+        # Print Module Comments
+        if self._module.meta.get('comments'):
+            print('Comments:')
+            for comment in self._module.meta['comments']:
+                prefix = '* '
+                if comment.startswith('\t'):
+                    prefix = self.SPACER+'- '
+                    comment = comment[1:]
+                print(f"{self.SPACER}{textwrap.fill(prefix+comment, 100, subsequent_indent=self.SPACER)}")
+            print('')
+
+
+    # =====================================================================================
+    # Command Do Functions: "options"
+    # =====================================================================================
+    def _do_options_list(self, params):
+        '''Shows the current context options'''
+        self._list_options(self._module._options)
+
+    def _do_options_set(self, params):
+        '''Sets a current context option'''
+
+        # Parse option key and value
+        option, value = self._parse_params(params)
+        if not (option and value):
+            self._help_options_set()
+            return
+
+        # Get Workspace
+        workspace = self._recon.get_current_workspace()
+
+        # Check option is a valid, known Module Option
+        options = self._module._options
+        option_name = option.upper()
+        if option_name in options:
+            options[option_name] = value
+            print(f"{option_name} => {value}")
+            workspace.set_config_property(option_name, self._module._fqn, options=options)
+        else:
+            self._console.error('Invalid option name.')
+
+    def _do_options_unset(self, params):
+        '''Unsets a current context option'''
+
+        # Parse option key and value
+        option, value = self._parse_params(params)
+        if not option:
+            self._help_options_unset()
+            return
+
+        # Check option is a valid, known Module Option
+        options = self._module._options
+        option_name = option.upper()
+        if option_name in options:
+            self._do_options_set(' '.join([option_name, 'None']))
+        else:
+            self._console.error('Invalid option name.')
+
+    # =====================================================================================
+    # Command Do Functions: "goptions"
+    # =====================================================================================
+    def do_goptions(self, params):
+        '''Manages the global context options'''
+
+        # Check goptions subcommand specified
+        if not params:
+            self.help_goptions()
+            return
+
+        # Execute goptions Command
+        arg, params = self._parse_params(params)
+        if arg in self._get_subcommands('goptions'):
+            return getattr(self, '_do_goptions_' + arg)(params)
+        else:
+            self.help_goptions()
+
+    def _do_goptions_list(self, params):
+        '''Shows the global context options'''
+        self._list_options()
+
+
+    def _do_goptions_set(self, params):
+        '''Sets a global context option'''
+
+        # Parse option key and value
+        option, value = self._parse_params(params)
+        if not (option and value):
+            self._help_goptions_set()
+            return
+
+        # Get Workspace
+        workspace = self._recon.get_current_workspace()
+
+        # Check option is a valid, known Global Option
+        goptions = self._recon.get_options()
+        option_name = option.upper()
+        if option_name in goptions:
+            goptions[option_name] = value
+            print(f"{option_name} => {value}")
+            workspace.set_config_property(option_name, options=goptions)
+        else:
+            self._console.error('Invalid option name.')
+
+    def _do_goptions_unset(self, params):
+        '''Unsets a global context option'''
+
+        # Parse option key and value
+        option, value = self._parse_params(params)
+        if not option:
+            self._help_goptions_unset()
+            return
+
+        # Check option is a valid, known Global Option
+        goptions = self._recon.get_options()
+        option_name = option.upper()
+        if option_name in goptions:
+            self._do_goptions_set(' '.join([option_name, 'None']))
+        else:
+            self._console.error('Invalid option name.')
+
+    # =====================================================================================
+    # Auto-completion Functions: goptions
+    # =====================================================================================
+    def complete_goptions(self, text, line, *ignored):
+        '''
+        Auto-completion for goptions commands
+
+        :param text: The subcommand text to auto-complete, which has been typed so far
+        :type text: str
+        :param line: The entire line that has been typed so far
+        :type line: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+        arg, params = self._parse_params(line.split(' ', 1)[1])
+        subs = self._get_subcommands('goptions')
+
+        # If directly matching sub-command found, auto-complete that
+        if arg in subs:
+            return getattr(self, '_complete_goptions_'+arg)(text, params)
+
+        # Else return all available matching commands
+        return [sub for sub in subs if sub.startswith(text)]
+
+    def _complete_goptions_list(self, text, *ignored):
+        '''
+        Auto-completion for goptions command: list
+        Placeholder: currently we have nothing more to provide for this command
+
+        :param text: The subcommand text to auto-complete, which has been typed so far
+        :type text: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+        return []
+
+    def _complete_goptions_set(self, text, *ignored):
+        '''
+        Auto-completion for goptions command: set
+        Searches all global options for an option that matches
+
+        :param text: The option name to auto-complete, which has been typed so far
+        :type text: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+        return [x for x in self._recon.get_options() if x.startswith(text.upper())]
+    # Auto-complete goptions "unset" in same way as set
+    _complete_goptions_unset = _complete_goptions_set
+
+    # =====================================================================================
+    # Auto-completion Functions: options
+    # =====================================================================================
+    def _complete_options_set(self, text, *ignored):
+        '''
+        Auto-completion for options command: set
+        Searches all Module options for an option that matches
+
+        :param text: The option name to auto-complete, which has been typed so far
+        :type text: str
+        :returns: List of matching subcommands, if found
+        :rtype: list
+        '''
+        return [x for x in self._module._options if x.startswith(text.upper())]
+    # Auto-complete options "unset" in same way as set
+    _complete_options_unset = _complete_options_set
+
+    # =====================================================================================
+    # Command Help Functions
+    # =====================================================================================
+    def help_goptions(self):
+        print(getattr(self, 'do_goptions').__doc__)
+        print(f"{os.linesep}Usage: goptions <{'|'.join(self._get_subcommands('goptions'))}> [...]{os.linesep}")
+
+    def _help_goptions_set(self):
+        print(getattr(self, '_do_goptions_set').__doc__)
+        print(f"{os.linesep}Usage: goptions set <option> <value>{os.linesep}")
+
+    def _help_goptions_unset(self):
+        print(getattr(self, '_do_goptions_unset').__doc__)
+        print(f"{os.linesep}Usage: goptions unset <option>{os.linesep}")
+
+    # =====================================================================================
+    # Internal Helpers
+    # =====================================================================================
