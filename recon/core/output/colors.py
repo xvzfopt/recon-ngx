3971d96a15bc81832320c1f8304131a6db317a3f
xvzf_opt <nsandro77@proton.me>
2026-07-25 00:30:05 +0100

    Added ANSI non-printable character designations for colours
    Without this, odd behaviour occurs with terminal history and auto-completion

--- recon/core/output/colors.py
+++ recon/core/output/colors.py
@@ -1,11 +1,13 @@
 # =====================================================================================
 # Console Output Colors
 # =====================================================================================
-COLOR_N             = '\033[m'  # native
-COLOR_R             = '\033[31m'  # red
-COLOR_G             = '\033[32m'  # green
-COLOR_G_BOLD        = '\033[1;32m'  # green
-COLOR_O             = '\033[33m'  # orange
-COLOR_B             = '\033[34m'  # blue
-COLOR_RNGX_BOLD     = '\033[1;38;2;100;218;222;49m' # Recon-NGX Branding Color
+# Note: The \001 and \002 ANSI character act as wrappers to tell readline that these are NON-PRINTABLE CHARACTERS!
+# These are required to ensure correct handling of command history, auto-completion etc. Otherwise... weird things happen
+COLOR_N             = '\001\033[m\002'  # native
+COLOR_R             = '\001\033[31m\002'  # red
+COLOR_G             = '\001\033[32m\002'  # green
+COLOR_G_BOLD        = '\001\033[1;32m\002'  # green
+COLOR_O             = '\001\033[33m\002'  # orange
+COLOR_B             = '\001\033[34m\002'  # blue
+COLOR_RNGX_BOLD     = '\001\033[1;38;2;100;218;222;49m\002' # Recon-NGX Branding Color
 
