0b9f8cbf010339c421e31cbd60c5179acc1e3cab
xvzf_opt <nsandro77@proton.me>
2026-07-25 00:37:11 +0100

    Added new Module/Dependency exceptions
    * Added exceptions for when module installation/uninstallation fails
    * Added exception for when Module Download fails

--- recon/core/exceptions.py
+++ recon/core/exceptions.py
@@ -13,3 +13,39 @@ class ValidationException(ReconNGXException):
     '''
     Validation Exception. Raised when validation fails
     '''
+    
+# =====================================================================================
+# Module Download Failure Exception
+# =====================================================================================
+class ModuleDownloadFailure(ReconNGXException):
+    '''
+    Raised when a module fails to download from the Module Marketplace
+    '''
+
+# =====================================================================================
+# Module Dependency Installation Failure
+# =====================================================================================
+class DependencyInstallationFailure(ReconNGXException):
+    '''
+    Raised when a Module dependency fails to install
+    '''
+
+    def __init__(self, requirement):
+        super(DependencyInstallationFailure, self).__init__(
+            "Dependency installation failed for requirement '%s'. Install this dependency manually and attempt module "
+            "installation again. For more details, Run Recon-NGX in verbose mode." % requirement
+        )
+
+# =====================================================================================
+# Module Dependency Uninstallation Failure
+# =====================================================================================
+class DependencyUninstallationFailure(ReconNGXException):
+    '''
+    Raised when a Module dependency fails to uninstall
+    '''
+
+    def __init__(self, package):
+        super(DependencyUninstallationFailure, self).__init__(
+            "Dependency uninstallation failed for package '%s'. Uninstall this dependency manually and attempt "
+            "module uninstallation again. For more details, Run Recon-NGX in verbose mode." % package
+        )
